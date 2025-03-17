e450847f4d3c781fe0eabea7e5e5b1b3fad8279a
Conor McGannon <conor.mcgannon@kellogg.northwestern.edu>
2025-03-17 21:49:44 +0000

    debugging cont

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    @current_user = User.find_by( "id" => session ["user_id"])
-    
-    puts "------------------ Running current_user ------------------"
+    puts "---- Running current_user ----"
+    @current_user = User.find_by({ "id" => session["user_id"] })
     puts "Session user_id: #{session["user_id"]}"
-    puts "Current User: #{@current_user.inspect}"
-    puts "------------------------------------"
+    puts "Current user: #{@current_user.inspect}"
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
+  
+  get  "/login"   => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get  "/logout"  => "sessions#destroy"
+  
   resources "entries"
   resources "places"
   resources "sessions"
